8a666692966a068bde171b495bb464b366c53abb
ultron89 <ultron89@KillerMach.Doom666>
2021-07-17 14:36:07 -0300

    Atualizando cliente.c

--- Server_And_Client/cliente.c
+++ Server_And_Client/cliente.c
@@ -10,20 +10,27 @@
 // Constante com a porta a ser usada:
 #define PORT 8080
 
+
 // MAIN func
 int main(int argc, char const *argv[])
 {
-	int sock = 0, valread;	
+	
 	// Chamando struct 'sockaddr_in', criando 'serv_addr';
-	struct sockaddr_in serv_addr;
+			struct sockaddr_in serv_addr;
 
-	char *hello = "Eae, aqui do cliente!!?";
-	char buffer[1024] = {0};
+	// Variáveis
+			int sock = 0, valread;						  // var para socket
+			char *hello = "Eae, aqui do cliente!!?"; // Mensagem para o servidor
+			char buffer[1024] = {0};                 // buffer para o read
 
-	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("\nErro na criação do socket\n");
-	}
+	// Criando socket
+			sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	// Verificando se ocorreu erro
+			if(sock < 0)
+			{
+				printf("\nErro na criação do socket\n");
+			}		
 
 	// usando a var com os atributos da struct 'sockaddr_in'		
 
@@ -33,10 +40,10 @@ int main(int argc, char const *argv[])
 	serv_addr.sin_port = 	htons(PORT);
 
 	// Converter endereços IPv4 e IPv6 de texto para binário
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)	 {
-		printf("\nEndereço inválido / Endereço não suportado\n");
-		return -1;
-	}
+			if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0){
+				printf("\nEndereço inválido / Endereço não suportado\n");
+				return -1;
+			}
 
 	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
 	
